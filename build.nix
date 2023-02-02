3c3c54af086b10f829cb4788841dd869331cf7ad
Jacek Galowicz <jacek@galowicz.de>
2023-02-02 21:50:59 +0100

    Add pre-commit-hooks

--- build.nix
+++ build.nix
@@ -3,7 +3,6 @@
 stdenv.mkDerivation {
   name = "whiteboard-opencv";
   src = ./.;
-  VERBOSE=1;
   nativeBuildInputs = [ cmake ];
   buildInputs = [ opencv4 ];
 
--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/4095b8232c4fb44aefce7aef8b3f106982e2b428";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, pre-commit-hooks, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
@@ -14,6 +15,16 @@
         };
         devShells.default = pkgs.mkShell {
           inputsFrom = [ config.packages.default ];
+          inherit (config.checks.pre-commit-check) shellHook;
+        };
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
+            };
+          };
         };
       };
     };
