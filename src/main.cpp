f0706d7266dde2b196e4370dad711ada062f2e78
Jacek Galowicz <jacek@galowicz.de>
2023-02-02 08:52:53 +0100

    Remove aruco-specific calls to also work with opencv 4.6.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <opencv2/aruco.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/objdetect/aruco_detector.hpp>
 #include <iostream>
 
 int main(int argc, char** argv) {
@@ -15,19 +13,8 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-
-  std::vector<int> markerIds;
-  std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-  auto detectorParams = cv::aruco::DetectorParameters();
-  auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-  cv::aruco::ArucoDetector detector(dictionary, detectorParams);
-  detector.detectMarkers(image, markerCorners, markerIds, rejectedCandidates);
-
-  auto outputImage = image.clone();
-  cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
-
   cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Display Image", outputImage);
+  cv::imshow("Display Image", image);
   cv::waitKey(0);
 
   return 0;
